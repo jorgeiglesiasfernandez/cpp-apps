b2abfb4e438f6ef93b642e73cfbef41b268218f6
Jorge Iglesias <jorge.iglesias@es.ibm.com>
2020-11-06 15:27:15 +0100

    first commit

---
+++ app-myapp-cpp/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author Jorge Iglesias/Spain/IBM@ES
+ * @description
+ * <p>
+ * Main app
+ * </p>
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "Hello Docker container!" << std::endl;
+  return 0;
+}
